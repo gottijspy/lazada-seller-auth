ea873a20aa961ae1bb81c38f606432fb014059f7
Pranith Gottigundala <gottijspy@gmail.com>
2020-08-07 17:31:18 +0530

    update user greeting

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -65,7 +65,7 @@ passport.use(
       console.log(profile);
 
       new User({
-        username: profile.account,
+        username: "lazop.sg@gmail.com",
         googleId: profile.account,
       })
         .save()
@@ -73,25 +73,6 @@ passport.use(
           console.log("new user created: " + newUser);
           done(null, newUser);
         });
-      // check if user already exists in our db
-      // User.findOne({ googleId: profile.id }).then((currentUser) => {
-      //   if (currentUser) {
-      //     // already have the user
-      //     console.log("user is: " + currentUser);
-      //     done(null, currentUser);
-      //   } else {
-      //     // if not, create user in our db
-      //     new User({
-      //       username: profile.displayName,
-      //       googleId: profile.id,
-      //     })
-      //       .save()
-      //       .then((newUser) => {
-      //         console.log("new user created: " + newUser);
-      //         done(null, newUser);
-      //       });
-      //   }
-      // });
     }
     // function (accessToken, refreshToken, profile, cb) {
     //   User.findOrCreate({ exampleId: profile.id }, function (err, user) {
