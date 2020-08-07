da579d057f008955e2d0521027f8fd284a12ace0
Pranith Gottigundala <gottijspy@gmail.com>
2020-08-07 17:00:04 +0530

    revert error logging for passport-oauth2

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -34,27 +34,28 @@ router.get("/lazada", passport.authenticate("oauth2"));
 // callback route for google to redirect to
 router.get(
   "/lazada/redirect",
+  passport.authenticate("oauth2"),
   // wrap passport.authenticate call in a middleware function
-  function (req, res, next) {
-    // call passport authentication passing the "local" strategy name and a callback function
-    passport.authenticate("oauth2", function (error, user, info) {
-      // this will execute in any case, even if a passport strategy will find an error
-      // log everything to console
-      console.log(error);
-      console.log(user);
-      console.log(info);
+  // function (req, res, next) {
+  //   // call passport authentication passing the "local" strategy name and a callback function
+  //   passport.authenticate("oauth2", function (error, user, info) {
+  //     // this will execute in any case, even if a passport strategy will find an error
+  //     // log everything to console
+  //     console.log(error);
+  //     console.log(user);
+  //     console.log(info);
 
-      if (error) {
-        res.status(401).send(error);
-      } else if (!user) {
-        res.status(401).send(info);
-      } else {
-        next();
-      }
+  //     if (error) {
+  //       res.status(401).send(error);
+  //     } else if (!user) {
+  //       res.status(401).send(info);
+  //     } else {
+  //       next();
+  //     }
 
-      res.status(401).send(info);
-    })(req, res);
-  },
+  //     res.status(401).send(info);
+  //   })(req, res);
+  // },
   (req, res) => {
     res.redirect("/profile/");
   }
